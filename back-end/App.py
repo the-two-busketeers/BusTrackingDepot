373d1158463b07056a9a4a0fb5003048dc53b86d
Oliver <oliver.dillon@wiltostech.com>
2019-08-22 20:52:22 +0100

    Update App.py
    - Fixed returning json

--- back-end/App.py
+++ back-end/App.py
@@ -10,10 +10,10 @@ def GetBusJson():
     dict = []
     for files in os.listdir( os.getcwd() + "/buses"):
         if files.endswith(".json"):
-            jsonFile = open(files)
+            jsonFile = open(os.getcwd() + "/buses" + files)
             jsonData = json.load(jsonFile)
             dict.Append(jsonData)
-    return dict
+    return json.dumps(dict)
 
 
 @app.route("/init", methods=["GET"])
