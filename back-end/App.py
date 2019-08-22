6d373752e45a1b92125771951534de4ef37a35c0
Oliver <oliver.dillon@wiltostech.com>
2019-08-22 20:33:06 +0100

    Dummy Data
    - Added dummy data return

--- back-end/App.py
+++ back-end/App.py
@@ -1,17 +1,27 @@
 
 from flask import *
+import os
+import json
 import modules.readingbusesapi as busWrapper
-
-
 
 app = Flask(__name__)
 
 @app.route("/init", methods=["GET"])
 def init():
-    return "hi"
-
+    
+    return GetBusJson()
 print(__name__)
 if __name__ == "__main__":
     # Making the app run on local host
     app.debug = True
     app.run(host= "0.0.0.0", port = 5000)
+
+
+def GetBusJson():
+    dict = []
+    for files in os.listdir("/buses"):
+        if files.endswith(".json"):
+            jsonFile = open(files)
+            jsonData = json.load(jsonFile)
+            dict.Append(jsonData)
+    return dict
