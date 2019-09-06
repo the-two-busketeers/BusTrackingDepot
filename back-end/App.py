ea67e7a30ce37657219eb33e7d1e0721d8ad765c
Oliver <oliver.dillon@wiltostech.com>
2019-09-06 19:13:34 +0100

    Update App.py
    - Added BusUpdater

--- back-end/App.py
+++ back-end/App.py
@@ -4,6 +4,67 @@ import os
 import json
 import readingbusesapi as busWrapper
 import datetime
+import threading
+import time
+
+
+class BusUpdater(object):
+
+    def __init__(self, interval=30):
+
+        self.interval = interval
+
+        self.busWrapper = busWrapper.ReadingBusesAPI("OHYrhd9WoJ")
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        while True:
+            buses = {}
+            aBuses = self.busWrapper.Call("Buses", {})
+
+            for data in aBuses:
+                buses[data["vehicle"]] = data
+            for files in os.listdir( os.getcwd() + "/buses"):
+                if files.endswith(".json"):
+                    filename = os.getcwd() + "/buses/" + files
+                    jsonFile = open(filename, 'r')
+                    jsonData = json.load(jsonFile)
+
+                    vehicle = jsonData["vehicle"]
+                    if vehicle in buses:
+                        busData = buses[vehicle]
+                        jsonData["service"] = busData["service"]
+                        jsonData["observed"] = busData["observed"]
+                        jsonData["isRunning"] = "1"
+
+                        buses[vehicle] = None
+                    else:
+                        jsonData["isRunning"] = "0"
+                        buses[vehicle] = None
+
+                    jsonFile.close()
+                    jsonFile = open(filename, "w")
+                    jsonFile.write(json.dumps(jsonData))
+                    jsonFile.close()
+            for da in buses:
+                data = buses[da]
+                if data != None:
+                    jsonData = {}
+                    jsonData["observed"] = data["observed"]
+                    jsonData["vehicle"] = data["vehicle"]
+                    jsonData["isRunning"] = "1"
+                    jsonData["service"] = data["service"]
+                    jsonData["returnTime"] = "2019-08-22 19:46:13"
+
+                    with open(os.getcwd() + "/buses/" + data["vehicle"] + ".json", 'w') as f:
+                        f.write(json.dumps(jsonData))
+            time.sleep(self.interval)
+
+example = BusUpdater()
+
 app = Flask(__name__)
 
 def GetBusJson():
@@ -18,7 +79,6 @@ def GetBusJson():
         if files.endswith(".json"):
             jsonFile = open(os.getcwd() + "/buses/" + files)
             jsonData = json.load(jsonFile)
-            print(jsonData)
             if jsonData["isRunning"] == "1":
                 dict["running"].append(jsonData)
             else:
