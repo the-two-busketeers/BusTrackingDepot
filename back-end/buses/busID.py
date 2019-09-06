15ee6bd06bc64b891803aec59b94b64d58cfe0d7
Hyprxz <88142@farnborough.ac.uk>
2019-09-06 18:36:54 +0100

    Added all fleetID JSON files

--- back-end/buses/busID.py
+++ back-end/buses/busID.py
@@ -1,2 +1,19 @@
 import json
 
+with open("bus_fleetID_list.txt", "r") as file:
+	fleetIDs = file.readlines()
+	fleetIDs = [x.strip() for x in fleetIDs]
+	print(fleetIDs)
+
+
+for ID in fleetIDs:
+	dictionary = {
+		"observed": "2019-08-22 18:46:13",
+		"vehicle": "1104",
+		"isRunning": "0",
+		"service": str(ID),
+		"returnTime": "2019-08-22 19:46:13"
+	}
+	file = open(str(ID) + ".json", "w")
+	file.write(json.dumps(dictionary))
+	file.close()
